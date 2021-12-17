71cf95b0ee769c289e87100b20c406775482082d
M F M Fazrin <mfmfazrin1986@gmail.com>
2021-12-17 13:50:07 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using IdentityNetCore.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace IdentityNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Policy = "MemberDep")]
+        public IActionResult Member()
+        {
+            return View();
+        }
+
+        [Authorize(Policy ="AdminDep")]
+        public IActionResult Admin()
+        {
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/IdentityController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using IdentityNetCore.Models;
+using IdentityNetCore.Service;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityNetCore.Controllers
+{
+    public class IdentityController : Controller
+    {
+
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IEmailSender emailSender;
+
+        public IdentityController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, RoleManager<IdentityRole> roleManager, IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            this._signInManager = signInManager;
+            this._roleManager = roleManager;
+            this.emailSender = emailSender;
+        }
+        public async Task<IActionResult> Signup()
+        {
+            var model = new SignupViewModel() {Role = "Member" };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Signup(SignupViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!(await _roleManager.RoleExistsAsync(model.Role)))
+                {
+                    var role = new IdentityRole {Name = model.Role };
+                    var roleResult = await _roleManager.CreateAsync(role);
+                    if (!roleResult.Succeeded)
+                    {
+                        var errors = roleResult.Errors.Select(s => s.Description);
+                        ModelState.AddModelError("Role", string.Join("," , errors));
+                        return View(model);
+                    }
+                }
+
+
+                if ((await _userManager.FindByEmailAsync(model.Email)) == null)
+                {
+                    var user = new IdentityUser { 
+                        Email= model.Email,
+                        UserName = model.Email
+                    };
+
+                   var result = await _userManager.CreateAsync(user, model.Password);
+                    if (result.Succeeded)
+                    {
+                        var claim = new Claim("Department", model.Department);
+                        await _userManager.AddClaimAsync(user, claim);
+                        await _userManager.AddToRoleAsync(user, model.Role);
+                        return RedirectToAction("Signin");
+                    }
+
+                    ModelState.AddModelError("Signup", string.Join("", result.Errors.Select(x => x.Description)));
+                    return View(model);
+                }
+            }
+
+            return View(model);
+        }
+
+
+        public async Task<IActionResult> ConfirmEmail(string userId, string token)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+
+            var result =  await _userManager.ConfirmEmailAsync(user, token);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Signin");
+            }
+
+            return new NotFoundResult();
+        }
+        public IActionResult Signin()
+        {
+            return View(new SigninViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Signin(SigninViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+
+                    var user = await _userManager.FindByEmailAsync(model.Username);
+
+                    var userClaims = await _userManager.GetClaimsAsync(user);
+
+                    if (await _userManager.IsInRoleAsync(user, "Member"))
+                    {
+                        return RedirectToAction("Member", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("Login", "Cannot login.");
+                }
+            }
+                return View(model);
+        }
+
+        public async Task<IActionResult> AccessDenied()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Signout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Signin");
+        }
+    }
+}
